dc0586d4f76656da493ab83be6e43f0712c3bc02
rushikesh3405 <98112083+rushikesh3405@users.noreply.github.com>
2022-10-21 14:44:31 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,62 @@
+const words = [
+    {
+        word : "addition",
+        hint : "The process of adding numbers"
+    },
+    {
+        word : "meeting",
+        hint : "Event in which people come together"
+    },
+
+    {
+        word : "substrat",
+        hint : "The process of minusing numbers"
+    },
+
+    {
+        word : "number",
+        hint : "Math symbol used for counting"
+    },
+    
+    {
+        word : "garden",
+        hint : "space for planting flwoer and plants"
+    },
+
+    {
+        word : "exchange",
+        hint : "The act of trading"
+    },
+
+    {
+        word : "delay",
+        hint : "getting late"
+    },
+
+    {
+        word : "mern",
+        hint : "mongodb,expressjs,react,node"
+    },
+
+    {
+        word : "lion",
+        hint : "king of forest"
+    },
+
+    {
+        word : "principal",
+        hint : "head of college"
+    },
+
+    {
+        word : "tacher",
+        hint : "marathi word guru"
+    }, 
+
+    {
+        word : "DSA",
+        hint : "computer science subject"
+    }
+
+
+]
